a7f5d52f3b808ab9c428069adb3a732dbf5e797c
Phinehas01 <phinehastech01@gmail.com>
2026-01-06 23:37:50 +0100

    App Bar titles

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/constants/colors.dart';
+import 'package:my_portfolio/constants/navButtons.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -14,7 +15,9 @@ class HomePage extends StatelessWidget {
           //MAIN
           Container(
             height: 60.0,
-            width: double.maxFinite,
+            margin: EdgeInsets.symmetric(
+              horizontal: 20.0, 
+              vertical: 10.0)  ,
             decoration: BoxDecoration(
               gradient: LinearGradient(
                 colors: [
@@ -26,10 +29,29 @@ class HomePage extends StatelessWidget {
             ),
             child: Row(
               children: [
-                Text("Phinex Digitals"),
+                Text("Phinex Digitals",
+                style: TextStyle(
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                  color: CustomColor.yellowSecondary,
+                ),
+                ),
                 Spacer(),
-                for (int i = 0; i < 5; i++)
-                  TextButton(onPressed: () {}, child: Text("Button")),
+                for (int i = 0; i < navTitles.length; i++)
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20.0),
+                    child: TextButton(
+                      onPressed: () {}, 
+                      child: Text(navTitles[i],
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                        color: CustomColor.whitePrimary,),
+                      
+                      ),
+                  ),
+                  ),
               ],
             ),
           ),
