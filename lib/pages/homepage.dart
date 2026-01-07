100838e3adea386cce508c041060ccedcace1cda
Phinehas01 <phinehastech01@gmail.com>
2026-01-07 17:49:25 +0100

    Menu tab

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/constants/colors.dart';
-import 'package:my_portfolio/constants/navButtons.dart';
+import 'package:my_portfolio/styles/style.dart';
+import 'package:my_portfolio/widgets/headerDesktop.dart';
+import 'package:my_portfolio/widgets/headerMobile.dart';
+import 'package:my_portfolio/widgets/siteLogo.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -13,47 +16,9 @@ class HomePage extends StatelessWidget {
         scrollDirection: Axis.vertical,
         children: [
           //MAIN
-          Container(
-            height: 60.0,
-            margin: EdgeInsets.symmetric(
-              horizontal: 20.0, 
-              vertical: 10.0)  ,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Colors.transparent,
-                  CustomColor.bgLight1,
-                ],
-                ),
-                borderRadius: BorderRadius.circular(100),
-            ),
-            child: Row(
-              children: [
-                Text("Phinex Digitals",
-                style: TextStyle(
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.bold,
-                  decoration: TextDecoration.underline,
-                  color: CustomColor.yellowSecondary,
-                ),
-                ),
-                Spacer(),
-                for (int i = 0; i < navTitles.length; i++)
-                  Padding(
-                    padding: const EdgeInsets.only(right: 20.0),
-                    child: TextButton(
-                      onPressed: () {}, 
-                      child: Text(navTitles[i],
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w500,
-                        color: CustomColor.whitePrimary,),
-                      
-                      ),
-                  ),
-                  ),
-              ],
-            ),
+         // HeaderDesktop(),
+          HeaderMobile(
+            onMenuTap: () {},
           ),
           //SKILLS
           Container(
